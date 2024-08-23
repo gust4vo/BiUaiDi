494cd585bef8d648b24ce10895914f5200dbc7ac
gust4vo <gust4vo.dev@gmail.com>
2024-08-22 23:03:56 -0300

    feat: passing file names on arguments

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@
 #include "QuadTree.hpp"  
 
 // Function to insert data from a file and return the QuadTree.
-QuadTree insertData(const std::string& filename) {
+QuadTree* insertData(const std::string& filename, int& size) {
     std::ifstream file(filename);
 
     std::string line;
     std::getline(file, line);  
 
-    int size = std::stoi(line); // Takes the number of Stations (or addresses) in the first line.
-    QuadTree Stations(4 * size - 3); // Explained on main().
+    size = std::stoi(line); // Takes the number of Stations (or addresses) in the first line.
+    QuadTree* Stations = new QuadTree(4 * size - 3);// Explained on main().
 
     while (std::getline(file, line)) {
         std::stringstream ss(line);
@@ -51,7 +51,8 @@ QuadTree insertData(const std::string& filename) {
         // Create a Point and Data object
         Point point(x, y);
         Data* data = new Data(point, neighborhood_name, street_type, street_name, region_name, address_id, property_number, street_id, zip_code, true);
-        Stations.insert(data);
+        Stations->insert(data);
+        std::cout << "inserido" << std::endl;
     }
 
     file.close();
@@ -60,6 +61,8 @@ QuadTree insertData(const std::string& filename) {
 }
 
 int main(int argc, char* argv[]) {
+    int size;
+
     std::string baseFile;
     std::string eventFile;
 
@@ -76,8 +79,8 @@ int main(int argc, char* argv[]) {
     // In the worst scenario, all nodes will be inserted in a occupped region, creating 4 empty nodes for 1 node.
     // This gives us a total of 4 * s - 3 nodes (s = number of recharging stations). 
     // Impossible situation in the real-world, but that's what it will be allocated in the QuadTree to ensure that there's sufficient space.
-    
-    QuadTree Stations = insertData(baseFile);
+
+    QuadTree* Stations = insertData(baseFile, size);
 
     std::ifstream file(eventFile);
     std::string line;
@@ -94,7 +97,7 @@ int main(int argc, char* argv[]) {
             iss >> x >> y >> k;
             MaxHeap heap(k);
             Point point(x, y);
-            Stations.knnSearch(point, heap);
+            Stations->knnSearch(point, heap);
 
             int neighborhoodSize = heap.getSize();
             Pair* results = new Pair[neighborhoodSize]; 
@@ -103,7 +106,7 @@ int main(int argc, char* argv[]) {
             // Print the k-nearest neighbors.
             for(int i = 0; i < neighborhoodSize; i++) {
                 Pair address = results[i];
-                std::cout << address.data->street_type << " " <<  address.data->street_name << ", " << 
+                std::cout << address.data->street_type << " " << address.data->street_name << ", " << 
                  address.data->property_number << ", "<< address.data->neighborhood_name << ", " 
                  << address.data->region_name << ", " << address.data->zip_code << 
                  " (" << std::fixed << std::setprecision(3) << distance(point, address.data->point) << ")" << std::endl;
@@ -115,14 +118,17 @@ int main(int argc, char* argv[]) {
         if (command == 'A' || command == 'D') {
             std::string id;
             iss >> id;
-            command == 'A' ? Stations.activate(id, true) : Stations.activate(id, false); 
+            command == 'A' ? Stations->activate(id, true) : Stations->activate(id, false); 
         }
     }
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
 
-    // std::cout << "Tempo de execução: " << elapsed.count() << " segundos" << std::endl;
+    std::cout << "Tempo de execução: " << elapsed.count() << " segundos" << std::endl;
+    std::ofstream times_file("tempos.txt", std::ios_base::app); 
+    times_file << "(" << size << ", " << std::fixed << std::setprecision(6) << elapsed.count() << 
+    ")," <<  std::endl;
 
     return 0;
 }
